b83b8a604892067a5c7ac52c30aea16638d2cb6f
Astro-Coco <colinc131@gmail.com>
2025-09-23 09:04:04 -0400

    organisation folders

---
+++ LAB2_A25/Fichiers Matlab/validation_laboratoire_2.m
@@ -0,0 +1,15 @@
+%%% Initialisation
+clc;
+clear;
+close all;
+
+%%% Organisation des repertoires
+%addpath('Aircraft/', 'Modules/');
+thisFileDir = fileparts(mfilename('fullpath'));
+addpath(fullfile(thisFileDir, 'Aircraft'));
+addpath(fullfile(thisFileDir, 'Modules')); 
+%% % Debut de vos etudes
+
+
+
+
