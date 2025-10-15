6b41f41a60cd0e11b83958a87d9caa1752757700
LambertVincent <lambert.vincent@etud.polymtl.ca>
2025-10-14 22:02:19 -0400

    Initialisation de la validation

--- Lab1_A25/Données Matlab/validation_laboratoire_1.m
+++ Lab1_A25/Données Matlab/validation_laboratoire_1.m
@@ -4,7 +4,7 @@ clear;
 close all;
 
 %%% Organisation des repertoires
-%addpath('Aircraft/', 'Modules/');
+addpath('Aircraft/', 'Modules/');
 thisFileDir = fileparts(mfilename('fullpath'));
 addpath(fullfile(thisFileDir, 'Aircraft'));
 addpath(fullfile(thisFileDir, 'Modules')); 
