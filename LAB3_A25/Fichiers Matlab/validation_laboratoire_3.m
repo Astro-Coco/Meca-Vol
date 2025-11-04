029ef82e5450238ee7bde9e9de25235c98f9d6a4
Astro-Coco <colinc131@gmail.com>
2025-11-04 10:58:19 -0500

    fix validation

--- LAB3_A25/Fichiers Matlab/validation_laboratoire_3.m
+++ LAB3_A25/Fichiers Matlab/validation_laboratoire_3.m
@@ -9,7 +9,6 @@ thisFileDir = fileparts(mfilename('fullpath'));
 addpath(fullfile(thisFileDir, 'Aircraft'));
 addpath(fullfile(thisFileDir, 'Modules'));
 %% % Debut de vos etudes
-%% % Debut de vos etudes
 avion = f_loadAircraftData;
 
 %%% Conditions de vol pour validation
@@ -19,8 +18,6 @@ masse_kg = 100000;
 zcg_m = 0;
 alpha_deg = 5;
 alpha_rad = m_convert.f_angle(alpha_deg, 'deg', 'rad');
-<<<<<<< HEAD
-=======
 alpha_dot = 0;
 theta_deg = 5;
 theta_rad = m_convert.f_angle(theta_deg, 'deg', 'rad');
@@ -51,6 +48,9 @@ mach_nb = m_atmos.f_nombre_mach(tas_mps, h_m);
     zcg_m, masse_kg, qbar_pa, fn_n, avion, alpha_rad, alpha_dot, ...
     q_radps, tas_mps, mach_nb, delev_rad, dflaps, dstab_rad)
 
+
+
+
 %% Configuration de l'avion (centrage)
 conditions.masse_kg  = m_convert.f_mass(13000, 'lbm', 'kg');
 conditions.Iyy_kgm2  = avion.inertie.Iyy_kgm2;
@@ -68,35 +68,74 @@ conditions.delev_rad = m_convert.f_angle(-0.5905, 'deg', 'rad');
 
 %% Définition des paramètres de vol
 conditions.q_radps   = 0;
->>>>>>> 0585a6cb9db91bec54114fb8903b32a5e7801194
 alpha_dot = 0;
-theta_deg = 5;
-theta_rad = m_convert.f_angle(theta_deg, 'deg', 'rad');
-Vt = 240;
-tas_mps = m_convert.f_velocity(Vt, 'kts', 'm/s');
-fn_n = 15000;
-xcg_perc = 0.1;
-q_radps = 0;
-delta_e = 0;
-delev_rad = 0;
-dflaps = 0;
-dstab_rad = 0;
-mach_nb = m_atmos.f_nombre_mach(tas_mps, h_m);
-qbar_pa = m_atmos.f_pression_dynamique(Vt, h_m);
+conditions.alpha_rad = m_convert.f_angle(7.4108, 'deg', 'rad');
+conditions.theta_rad = conditions.alpha_rad;   % vol symétrique: θ ≈ α en palier
 
+%% Configuration des moteurs
+conditions.fn_n      = 3.3320e+04;   % [N]
 
+mach_nb = m_atmos.f_nombre_mach(conditions.tas_mps, conditions.altitude_m);
+qbar_pa = m_atmos.f_pression_dynamique(conditions.tas_mps, conditions.altitude_m);
 %%% Coefficients dans le repère stabilité
-%[cls, cds, cms] = m_aero.f_coeff_stabilite(alpha_rad, alpha_dot, ...
-%    q_radps, tas_mps, mach_nb, qbar_pa, delta_e, dflaps, dstab_rad, ...
-%    fn_n, avion);
-%
-%%%% Coefficients dans le repère body
-%[clb, cdb, cmb] = m_aero.f_stab2body(cls, cds, cms, alpha_rad)
+[cls, cds, cms] = m_aero.f_coeff_stabilite( ...
+    conditions.alpha_rad, alpha_dot, conditions.q_radps, ...
+    conditions.tas_mps, mach_nb, qbar_pa, ...
+    conditions.delev_rad, conditions.dflaps, conditions.dstab_rad, ...
+    conditions.fn_n, avion);
+
+%%% Coefficients dans le repère body
+[clb, cdb, cmb] = m_aero.f_stab2body(cls, cds, cms, conditions.alpha_rad);
 
 
 %%% Forces et moment
-clb = 0.7183;
-cdb = -0.0220;
-cmb = -0.1303;
-[fx_n, fz_n, my_nm] = m_edm.f_forces(clb, cdb, cmb, theta_rad, xcg_perc, ...
-    zcg_m, masse_kg, qbar_pa, fn_n, avion)
+[fx_n, fz_n, my_nm] = m_edm.f_forces(clb, cdb, cmb, conditions.theta_rad, ...
+    conditions.xcg_perc, conditions.zcg_m, conditions.masse_kg, ...
+    qbar_pa, conditions.fn_n, avion, conditions.alpha_rad, alpha_dot, ...
+    conditions.q_radps, conditions.tas_mps, mach_nb, ...
+    conditions.delev_rad, conditions.dflaps, conditions.dstab_rad);
+
+
+ub0   = conditions.tas_mps * cos(conditions.alpha_rad);
+wb0   = conditions.tas_mps * sin(conditions.alpha_rad);
+q0    = conditions.q_radps;        % = 0 à l’init (page 10)
+theta0= conditions.theta_rad;      % = alpha_rad à l’init (palier)
+h0    = conditions.altitude_m;
+
+x0 = [ub0; wb0; q0; theta0; h0];
+
+[time, x] = m_edm.f_simuler_avion(x0, 50, 1.0, conditions, avion, []);
+
+% time = t;  x = X;
+
+% Affichage du résultat de la simulation
+figure;
+subplot(3, 2, [1 2]);
+plot(time, m_convert.f_length(x(:, 5), 'm', 'ft')); grid on; box on;
+xlabel('Temps [sec]'); ylabel('Altitude [ft]');
+set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on', 'Xlim', [0 50], ...
+         'Ylim', [15000 25000]);
+
+subplot(3, 2, 3);
+plot(time, m_convert.f_velocity(x(:, 1), 'm/s', 'kts')); grid on; box on;
+set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on', 'Xlim', [0 50], ...
+         'Ylim', [200 300]);
+ylabel('u_b [kts]');
+
+subplot(3, 2, 4);
+plot(time, m_convert.f_velocity(x(:, 2), 'm/s', 'kts')); grid on; box on;
+set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on', 'Xlim', [0 50], ...
+         'Ylim', [200 401]);
+ylabel('w_b [kts]');
+
+subplot(3, 2, 5);
+plot(time, m_convert.f_angle(x(:, 3), 'rad', 'deg')); grid on; box on;
+set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on', 'Xlim', [0 50], ...
+         'Ylim', [-5 5]);
+xlabel('Temps [sec]'); ylabel('q [deg/s]');
+
+subplot(3, 2, 6);
+plot(time, m_convert.f_angle(x(:, 4), 'rad', 'deg')); grid on; box on;
+set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on', 'Xlim', [0 50], ...
+         'Ylim', [0 10]);
+xlabel('Temps [sec]'); ylabel('\theta [deg]');
